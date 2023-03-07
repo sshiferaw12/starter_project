9eae2b24339c541dbe869c4e035793b44908ddac
Solomon Shiferaw <solomon_shiferaw@faculty.ie.edu>
2023-03-07 04:12:53 +0100

    Adding db

--- app.py
+++ app.py
@@ -1,6 +1,35 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.secret_key = "session_key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'my_secret_key'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+
+# with app.app_context():
+#       db.create_all()
+#       usr = User("sola","pp")
+#       usr1 = User("bab","ppdf")
+#       usr2 = User("df","fd")
+#       db.session.add(usr1)
+#       db.session.add(usr2)
+#       db.session.commit()
 
 @app.route('/')
 def index():
@@ -27,25 +56,31 @@ def loginPage():
 
 @app.route("/handle-login", methods=["POST"])
 def handleLogin():
+
     username = request.form["username"]
     password = request.form["password"]
 
-    if ((username == "abd") and (password == "pass") ): 
+    userFound = User.query.filter_by(username=username,password=password).first()   
+    
+    if (userFound): 
+        session['username'] = username
         return redirect(url_for('profile'))
     else: 
         message = 'Invalid login credentials. Please try again.'
         return render_template('login.html', message=message)
 
-
 @app.route('/profile')
 def profile():
+    if ('username' in session):
         return render_template('profile.html')
- 
+    else:
+        return render_template('login.html',message="Hey you are not allowed here, you need to login!")
 
 @app.route('/logout')
 def logout():
+    session.pop('username')
     return redirect(url_for('loginPage'))
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,)
